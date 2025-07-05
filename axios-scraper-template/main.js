84a6048d59161b65e83e71006645e1a54c03faff
Gitnaseem745 <mrnaseem745@gmail.com>
2025-07-05 23:12:30 +0530

    Implement web scraper using axios and cheerio

---
+++ axios-scraper-template/main.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const siteUrl = "";
+
+async function scraper(startUrl) {
+    let url = startUrl;
+    const scrapedContent = [];
+
+    try {
+        while (url) {
+            const { data } = await axios.get(url, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/119.0.0.0 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Referer': 'https://google.com',
+                    'DNT': '1',
+                    'Connection': 'keep-alive',
+                    'Upgrade-Insecure-Requests': '1',
+                }
+            });
+            const $ = cheerio.load(data);
+
+            // update this to match your target HTML
+            $('.cotent-item').each((_, el) => {
+                const title = $(el).find('h2').text().trim();
+                const description = $(el).find('p').text().trim();
+
+                scrapedContent.push({
+                    title,
+                    description
+                })
+            })
+
+            // handle pagination
+            const nextPage = $('.pager .next a').attr('href');
+            if (nextPage) {
+                url = siteUrl + nextPage;
+            } else {
+                url = null; // No more pages
+            }
+        };
+
+        // save to file
+        fs.writeFileSync('scrapped.json', JSON.stringify(scrapedContent, null, 2), 'utf-8');
+        console.log('All content scraped successfully.');
+    } catch (err) {
+        console.log("Error occured:", err.message);
+    }
+}
+
+scraper(siteUrl);
